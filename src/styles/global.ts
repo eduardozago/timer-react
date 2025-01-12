e98cc747a8a3a4c29fdabd40c63614ef4c47e43b
Eduardo <zagoe22@gmail.com>
2025-01-12 16:38:56 -0300

    feat: add header and styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,6 +13,8 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
+        background: ${props => props.theme["gray-900"]};
+        color: ${props => props.theme["gray-300"]};
     }
 
     body, input, textarea, button {
